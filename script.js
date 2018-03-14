8619ce345d58cf493cc8bf674f7ced955a6d212a
xxx87 <xxx_87@ukr.net>
2018-03-14 23:19:27 +0200

    File script.js added

---
+++ script.js
@@ -0,0 +1,8 @@
+/***  Задача по String Reverse  ***/
+function strReverse(string) {
+    return string.split('').reverse().join(''); /* split - превращает строку в массив, reverse - меняет порядок элементов в массиве на противоположный, join - слкеивает массив обратно в строку */
+}
+strReverse('Hello');
+/**********************************/
+
+
